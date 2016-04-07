d147c33f8cf54aa6a5e306d9a7c3b036fdd4141a
marthauk <martinhaukali@gmail.com>
2016-04-07 12:42:13 +0200

    Added files via upload

--- Elevator_Project/controlModule/control_module.go
+++ Elevator_Project/controlModule/control_module.go
@@ -3,28 +3,33 @@ package controlModule
 import(
 "net"
 ."./network"
+"strconv"
 )
+const port int = 40000;//setting the portnumber. Selected a random port
 
 type elev struct {
 	self_id int
 	self_IP string
 	elevators map[int]*Elevator
 	external_orders [2][N_FLOORS]int
-	master int
+	master int 
 }
 
 //initializing of elevator
-func initialize_elev() elev{
+func Initialize_elev() elev{
 	var e elev
 	e.elevators = make(map[int]*Elevator) 
 
+
+	//initialize message here ?
 	//initialize driver
 	addr,_ :=net.InterfaceAddrs()
 	tempVar:=addr[1]
  	ip:=tempVar.String()
 	e.self_IP=ip[0:15]
-	e.self_id := int(addr[1].String()[12]-'0')*100 + int(addr[1].String()[13]-'0')*10 + int(addr[1].String()[14]-'0')
+	e.self_id := int(addr[1].String()[12]-'0')*100 + int(addr[1].String()[13]-'0')*10 + int(addr[1].String()[14]-'0')//this will work for IP-addresses of format ###.###.###.###, but not with only for ###.###.###.##/
 	e.elevators[e.self_id]=new(Elevator)
+
 
 
 	e.set_master()
@@ -32,21 +37,24 @@ func initialize_elev() elev{
 
 }
 
-func initialize_connections(){
+func Initialize_connections(){
+ 	var isMaster bool =false
+ 	var tempIP string= e.self_IP[0:12]
+ 	var masterIP string ="255.255.255.255"
+ 	masterIP=tempIP + strconv.Itoa(e.master)
  	if e.self_id==e.master
  		{	//the current elevator is master, needs master settings
  				//go masterUDPSEND
  				//go masterUDPLISTEN
-
+ 				isMaster=true
+ 				
  		}
-	else
-	 { //the current elevator is slave, needs slave settings for udp
-	 				//go slaveUDPSEND
- 				//go slaveUDPLISTEN
-		}
+ 	UDP_initialize(isMaster,port,masterIP)
+	
+	
 }
 
-func set_master(){
+func Set_master(){
 	// checking which elevator has the highest IP to determine master 
 	max :=0
 	for i,_ :=range(e.elevators){
